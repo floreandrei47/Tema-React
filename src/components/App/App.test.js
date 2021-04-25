f5215d223df657530698878b1c12060ede00e489
Florea Andrei <floreandrei47@gmail.com>
2021-04-25 23:31:52 +0300

    Testing for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,17 @@
+import { shallow } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const store = configureStore();
+describe('App', () => {
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+   shallow(<App store={store}/>);
+});
+
+it('to contain something',()=>{
+  const app =<App store={store}/>;
+  expect.anything(app);
+});
+
 });
